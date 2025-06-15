63909637c1722d7f12e0b3e497b94f34823a0515
chimaihueze <kihueze@gmail.com>
2025-06-15 16:52:17 +0100

    modified function, removed class Requester

--- resilient_requester/resilient_requester.py
+++ resilient_requester/resilient_requester.py
@@ -3,21 +3,18 @@ import requests
 from resilient_requester.helpers.helper_functions import check_params, handle_exception
 
 
-class Requester:
+def get_request(url, proxy=None, headers=None, timeout=0, retries=0):
+    proxy, headers, timeout, retries = check_params(proxy, headers, timeout, retries)
+    attempts = 0
 
-    @staticmethod
-    def get_request(url, proxy=None, headers=None, timeout=0, retries=0):
-        proxy, headers, timeout, retries = check_params(proxy, headers, timeout, retries)
-        attempts = 0
-
-        while attempts <= retries:
-            try:
-                logging.info(f"Sending a GET REQUEST: {url}")
-                response = requests.get(url=url, proxies=proxy, headers=headers, timeout=timeout)
-                response.raise_for_status()
-                logging.info(f"Request successful: {response.status_code}.")
-                return response
-            except Exception as e:
-                attempts = handle_exception(e, attempts, retries)
-                if not attempts: return None
-        return None
+    while attempts <= retries:
+        try:
+            logging.info(f"Sending a GET REQUEST: {url}")
+            response = requests.get(url=url, proxies=proxy, headers=headers, timeout=timeout)
+            response.raise_for_status()
+            logging.info(f"Request successful: {response.status_code}.")
+            return response
+        except Exception as e:
+            attempts = handle_exception(e, attempts, retries)
+            if not attempts: return None
+    return None
